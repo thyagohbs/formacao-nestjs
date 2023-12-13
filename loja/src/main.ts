cfd4e9b2313851614ad9d109516b961b2352b28a
Thyago Henrique <thyagobs.al@gmail.com>
2023-12-13 12:14:05 -0300

    update

--- loja/src/main.ts
+++ loja/src/main.ts
@@ -6,9 +6,9 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(
     new ValidationPipe({
-      transform: true,
-      whitelist: true,
-      forbidNonWhitelisted: true,
+      transform: true, // Para transformar o JSON
+      whitelist: true, // ignorar todas as propriedades que vierem no JSON que não estiverem no DTO 
+      forbidNonWhitelisted: true, // Para lançar um erro se alguém mandar um dado no JSON que não está no DTO
     }),
   );
   await app.listen(3000);
