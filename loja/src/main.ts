05fc76740751ff3682db2557411741afa9be67aa
Thyago Henrique <thyagobs.al@gmail.com>
2023-12-13 19:46:22 -0300

    update

--- loja/src/main.ts
+++ loja/src/main.ts
@@ -7,7 +7,7 @@ async function bootstrap() {
   app.useGlobalPipes(
     new ValidationPipe({
       transform: true, // Para transformar o JSON
-      whitelist: true, // ignorar todas as propriedades que vierem no JSON que não estiverem no DTO 
+      whitelist: true, // ignorar todas as propriedades que vierem no JSON que não estiverem no DTO
       forbidNonWhitelisted: true, // Para lançar um erro se alguém mandar um dado no JSON que não está no DTO
     }),
   );
