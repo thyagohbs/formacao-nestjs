1e43ab9a31a0778b212c29343440a747106c8374
Thyago Henrique <thyagobs.al@gmail.com>
2023-12-27 10:46:54 -0300

    commit

--- 02_loja-orm-api/src/pedido/pedido.module.ts
+++ 02_loja-orm-api/src/pedido/pedido.module.ts
@@ -1,8 +1,12 @@
 import { Module } from '@nestjs/common';
 import { PedidoService } from './pedido.service';
 import { PedidoController } from './pedido.controller';
+import { TypeOrmModule } from '@nestjs/typeorm';
+import { PedidoEntity } from './pedido.entity';
+import { UsuarioEntity } from '../usuario/usuario.entity';
 
 @Module({
+  imports: [TypeOrmModule.forFeature([PedidoEntity, UsuarioEntity])],
   controllers: [PedidoController],
   providers: [PedidoService],
 })
