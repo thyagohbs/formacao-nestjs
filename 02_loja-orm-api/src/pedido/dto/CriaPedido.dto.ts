c4dc6323a1b14924cb33cacf2d8fbe5832b5db5f
Thyago Henrique <thyagobs.al@gmail.com>
2023-12-27 15:42:37 -0300

    update

--- 02_loja-orm-api/src/pedido/dto/CriaPedido.dto.ts
+++ 02_loja-orm-api/src/pedido/dto/CriaPedido.dto.ts
@@ -1,12 +1,10 @@
 import { Type } from 'class-transformer';
 import { ArrayMinSize, IsArray, IsInt, ValidateNested } from 'class-validator';
 
-
 class ItemPedidoDTO {
   @IsInt()
   quantidade: number;
 }
-
 
 export class CriaPedidoDTO {
   @ValidateNested()
